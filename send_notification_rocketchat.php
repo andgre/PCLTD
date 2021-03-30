b414a2245dc227b612b0f6d6364130d268ff3774
andgre <andrey.gresko@gmail.com>
2021-03-30 13:55:28 +0300

    commit

--- send_notification_rocketchat.php
+++ send_notification_rocketchat.php
@@ -6,6 +6,7 @@ $user    = "root";
 $pass    = "zGBQhE2HUe";
 $db_name = "otrs";
 $conn = new mysqli($host, $user, $pass, $db_name);
+if (!$conn) { die ("error: " . mysqli_error() . PHP_EOL); }
 mysqli_set_charset($conn, "utf8");
 $result = $conn->query("SELECT * FROM `temp`");
 while($arr[] = mysqli_fetch_assoc($result));
@@ -14,8 +15,7 @@ while ($curr_row = current($arr)) {
     exec ('curl -X POST -H \'Content-Type: application/json\' --data \'{"text":"'.$curr_row['tn'].'","attachments":[{"title":"'.$curr_row['create_time'].'","text":"'.$curr_row['title'].'","color":"#764FA5"}]}\' http://10.100.1.59:3000/hooks/9AXaRz5uTDkoz32oW/NFCcuGNyv8pLWcqDDiWbopDAgBHB3f7W33Ws7uQaQXkJtoXt > /dev/null 2>&1');
 //      echo 'curl -X POST -H \'Content-Type: application/json\' --data \'{"text":"'.$curr_row['tn'].'","attachments":[{"title":"'.$curr_row['create_time'].'","text":"'.$curr_row['title'].'","color":"#764FA5"}]}\' http://10.100.1.59:3000/hooks/9AXaRz5uTDkoz32oW/NFCcuGNyv8pLWcqDDiWbopDAgBHB3f7W33Ws7uQaQXkJtoXt'.'\n';
     $result =$conn->query("DELETE FROM  `temp` where tn = '".$curr_row['tn']."'");
-    if (!$result) {
-        die('Ошибка выполнения запроса:' . mysql_error());}
+    if (!$result) {  die("error: " . mysqli_error() . PHP_EOL);}
     /*
             while (false !== ($curr_field = current($curr_row))) {
             //echo $curr_field."\n";
@@ -25,5 +25,5 @@ while ($curr_row = current($arr)) {
     //echo "!";
     next($arr);
 }
-$conn->close()
+$conn->close();
 ?>
